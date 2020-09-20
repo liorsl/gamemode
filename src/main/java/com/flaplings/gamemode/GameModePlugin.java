e2dac1a0cf26d8da44cf542940f7c7b307d5e307
Lior <liorsl@protonmail.com>
2020-09-20 18:12:12 +0300

    Remove startup messages :(

--- src/main/java/com/flaplings/gamemode/GameModePlugin.java
+++ src/main/java/com/flaplings/gamemode/GameModePlugin.java
@@ -12,19 +12,13 @@ public final class GameModePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Loading configuration...");
         config = new GameModePluginConfiguration(this, new File(getDataFolder(), "config.yml"), getConfig());
-        getLogger().info("Done loading configuration");
-
-        getLogger().info("Registering commands");
 
         register(null, "gamemode");
         register(GameMode.CREATIVE, "gmc");
         register(GameMode.SURVIVAL, "gms");
         register(GameMode.ADVENTURE, "gma");
         register(GameMode.SPECTATOR, "gmsp");
-
-        getLogger().info("Done loading " + getDescription().getName());
     }
 
     private void register(GameMode defaultGameMode, String alias) {
